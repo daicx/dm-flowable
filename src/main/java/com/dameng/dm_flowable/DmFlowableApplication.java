884cfcd20ba0d98b98e776cdb0c54d7bb0087c53
daichangxia <daichangxia>
2025-05-12 16:30:47 +0800

    1

---
+++ src/main/java/com/dameng/dm_flowable/DmFlowableApplication.java
@@ -0,0 +1,13 @@
+package com.dameng.dm_flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmFlowableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dameng/dm_flowable/DmFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dameng.dm_flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
